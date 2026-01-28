9e6c6a89b78b37bd0f1e337331b2abf501dd8454
Removel <Removel0202@163.com>
2026-01-28 15:29:28 +0800

    fix：修复139当中代码‘数组越界’的不规范问题，使代码更加规范符合逻辑

--- 139_单词拆分.cpp
+++ 139_单词拆分.cpp
@@ -32,7 +32,7 @@ dp[i+word.size()]=dp[i];
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<bool>dp(s.size(),false);
+        vector<bool>dp(s.size()+1,false);
         dp[0]=true;
         for(int i=0;i<s.size();++i){
             for(auto& word : wordDict){
